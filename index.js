7acf250de85f41f0f509edc2d0e82714be64c5f7
KacperSliwinskii <55840160+KacperSliwinskii@users.noreply.github.com>
2020-04-15 13:38:53 +0100

    test4

--- index.js
+++ index.js
@@ -1,2 +1,68 @@
+function myFunction0() {
+  var x = document.getElementById("myDIV0");
+  if (x.innerHTML === "Cat") {
+    x.innerHTML = "Kot";
+  } else {
+    x.innerHTML = "Cat";
+  }
+}
+
+
+
+
+
+
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.innerHTML === "Dog") {
+    x.innerHTML = "Pies";
+  } else {
+    x.innerHTML = "Dog";
+  }
+}
+
+
+
+
+
+
+
+function myFunction2() {
+  var x = document.getElementById("myDIV2");
+  if (x.innerHTML === "Hamster") {
+    x.innerHTML = "Chomik";
+  } else {
+    x.innerHTML = "Hamster";
+  }
+}
+
+function myFunction3() {
+  var x = document.getElementById("myDIV3");
+  if (x.innerHTML === "Bear") {
+    x.innerHTML = "Niedźwiedź";
+  } else {
+    x.innerHTML = "Bear";
+  }
+}
+
+function myFunction4() {
+  var x = document.getElementById("myDIV4");
+  if (x.innerHTML === "Horse") {
+    x.innerHTML = "Koń";
+  } else {
+    x.innerHTML = "Horse";
+  }
+}
+
+
+function myFunction5() {
+  var x = document.getElementById("myDIV5");
+  if (x.innerHTML === "Monkey") {
+    x.innerHTML = "Małpa";
+  } else {
+    x.innerHTML = "Monkey";
+  }
+}
 
 
